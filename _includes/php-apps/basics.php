84a41068ef260de7f5098b9279e088c12b2d4f0e
Scott Mahr <essmahr@gmail.com>
2017-01-18 20:38:06 -0800

    Add PSR-2 documentation

---
+++ _includes/php-apps/basics.php
@@ -0,0 +1,28 @@
+<?php
+namespace Vendor\Package;
+
+use FooInterface;
+use BarClass as Bar;
+use OtherVendor\OtherPackage\BazClass;
+
+class Foo extends Bar implements FooInterface
+{
+
+  const SAMPLE_CONSTANT = 12345678;
+
+  public function sampleMethod($a, $b = null)
+  {
+    if ($a === $b) {
+      bar();
+    } elseif ($a > $b) {
+      $foo->bar($arg1);
+    } else {
+      BazClass::bar($arg2, $arg3);
+    }
+  }
+
+  final public static function bar()
+  {
+    // method body
+  }
+}
